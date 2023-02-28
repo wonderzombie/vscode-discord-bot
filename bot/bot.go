43d331db2077c1f2459278a5d546fd04dcc73155
wonderzombie <wonderzombie@gmail.com>
2023-02-28 15:46:39 -0800

    Split handlers from bot.go.
    Add v small struct for bot.

--- bot/bot.go
+++ bot/bot.go
@@ -1,13 +1,7 @@
 package bot
 
 import (
-	"fmt"
 	"log"
-	"strings"
-	"sync"
-	"time"
-
-	"github.com/wonderzombie/godiscbot/combat"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -15,20 +9,25 @@ import (
 type MessageHandler func(*discordgo.Session, *discordgo.MessageCreate)
 
 var (
-	seenList   = make(map[string]time.Time)
-	seenListMx = sync.Mutex{}
-	sentList   = make([]*discordgo.Message, 20)
-
 	botName = ""
-
-	Handlers = []MessageHandler{
-		pong,
-		seeing,
-		hasSeen,
-		sent,
-		combat.Handler,
-	}
 )
+
+type Bot struct {
+	dg *discordgo.Session
+}
+
+func New(dg *discordgo.Session, ready MessageHandler) *Bot {
+	dg.AddHandlerOnce(ready)
+	return &Bot{
+		dg: dg,
+	}
+}
+
+func (b *Bot) AddHandlers(handlers []MessageHandler) {
+	for _, h := range handlers {
+		b.dg.AddHandler(skipSelf(h))
+	}
+}
 
 func AddMessageHandlers(dg *discordgo.Session) {
 	dg.AddHandlerOnce(ready)
@@ -48,79 +47,4 @@ func skipSelf(fn MessageHandler) MessageHandler {
 			fn(s, m)
 		}
 	}
-}
-
-func pong(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Content == "!ping" {
-		s.ChannelMessageSend(m.ChannelID, "PONG")
-	} else if m.Content == "!pong" {
-		s.ChannelMessageSend(m.ChannelID, "PING")
-	}
-}
-
-// TODO: something like a list. Maybe confined to an allow list of channels in .env or similar.
-func seeing(s *discordgo.Session, m *discordgo.MessageCreate) {
-	seenListMx.Lock()
-	defer seenListMx.Unlock()
-	if _, ok := seenList[m.Author.Username]; !ok {
-		seenList[m.Author.Username] = m.Timestamp
-	}
-}
-
-func hasSeen(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if !strings.HasPrefix(m.Content, "!seen") {
-		return
-	}
-
-	seenListMx.Lock()
-	defer seenListMx.Unlock()
-
-	out := seenResponse(m)
-	if out == "" {
-		return
-	}
-
-	msg, err := s.ChannelMessageSend(m.ChannelID, out)
-	if err != nil {
-		log.Printf("error sending message: %v\ncontent was: %s", err, out)
-		return
-	}
-	sentList = append(sentList, msg)
-}
-
-func seenResponse(m *discordgo.MessageCreate) string {
-	var out string
-	words := strings.Fields(m.Content)
-	if len(words) == 1 {
-		out = fmt.Sprintf("%v", seenList)
-	} else if len(words) == 2 {
-		username := words[1]
-		if t, ok := seenList[username]; ok {
-			out = fmt.Sprintf("%s, I saw %s and it was %v", m.Message.Author, username, t)
-		} else {
-			out = fmt.Sprintf("%s, I've never seen %s", m.Message.Author, username)
-		}
-	}
-	return out
-}
-
-func sent(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if !strings.HasPrefix(m.Content, "!sent") {
-		return
-	}
-
-	if len(sentList) == 0 {
-		s.ChannelMessageSend(m.ChannelID, "i ... haven't seen anyone")
-		return
-	}
-
-	var buf strings.Builder
-
-	buf.WriteString("I sent these:\n")
-	for i, msg := range sentList {
-		out := fmt.Sprintf("%d %s %s %s\n", i, msg.ChannelID, msg.Timestamp, msg.Content)
-		buf.WriteString(out)
-	}
-
-	s.ChannelMessageSend(m.ChannelID, buf.String())
 }
---
+++ bot/handlers.go
@@ -0,0 +1,99 @@
+package bot
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func pong(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Content == "!ping" {
+		s.ChannelMessageSend(m.ChannelID, "PONG")
+	} else if m.Content == "!pong" {
+		s.ChannelMessageSend(m.ChannelID, "PING")
+	}
+}
+
+var (
+	seenList   map[string]time.Time
+	seenListMx sync.Mutex
+	sentList   []*discordgo.Message
+
+	Handlers = []MessageHandler{
+		pong,
+		seeing,
+		hasSeen,
+		sent,
+	}
+)
+
+// TODO: something like a list. Maybe confined to an allow list of channels in .env or similar.
+func seeing(s *discordgo.Session, m *discordgo.MessageCreate) {
+	seenListMx.Lock()
+	defer seenListMx.Unlock()
+	if _, ok := seenList[m.Author.Username]; !ok {
+		seenList[m.Author.Username] = m.Timestamp
+	}
+}
+
+func hasSeen(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if !strings.HasPrefix(m.Content, "!seen") {
+		return
+	}
+
+	seenListMx.Lock()
+	defer seenListMx.Unlock()
+
+	out := seenResponse(m)
+	if out == "" {
+		return
+	}
+
+	msg, err := s.ChannelMessageSend(m.ChannelID, out)
+	if err != nil {
+		log.Printf("error sending message: %v\ncontent was: %s", err, out)
+		return
+	}
+	sentList = append(sentList, msg)
+}
+
+func seenResponse(m *discordgo.MessageCreate) string {
+	var out string
+	words := strings.Fields(m.Content)
+	if len(words) == 1 {
+		out = fmt.Sprintf("%v", seenList)
+	} else if len(words) == 2 {
+		username := words[1]
+		if t, ok := seenList[username]; ok {
+			out = fmt.Sprintf("%s, I saw %s and it was %v", m.Message.Author, username, t)
+		} else {
+			out = fmt.Sprintf("%s, I've never seen %s", m.Message.Author, username)
+		}
+	}
+	return out
+}
+
+func sent(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if !strings.HasPrefix(m.Content, "!sent") {
+		return
+	}
+
+	if len(sentList) == 0 {
+		s.ChannelMessageSend(m.ChannelID, "i ... haven't seen anyone")
+		return
+	}
+
+	var buf strings.Builder
+
+	buf.WriteString("I sent these:\n")
+	for i, msg := range sentList {
+		out := fmt.Sprintf("%d %s %s %s\n", i, msg.ChannelID, msg.Timestamp, msg.Content)
+		buf.WriteString(out)
+	}
+
+	s.ChannelMessageSend(m.ChannelID, buf.String())
+}
